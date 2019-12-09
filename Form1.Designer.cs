e1758ab7d845372a442ef72db38ce9c80c085ef7
abdu <bluemanbusiness@gmail.com>
2019-12-09 21:59:08 +0100

    first test

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Afficher";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(102, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.blackToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.colorToolStripMenuItem.Text = "color";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.redToolStripMenuItem.Text = "red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // blackToolStripMenuItem
+            // 
+            this.blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            this.blackToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.blackToolStripMenuItem.Text = "black";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(739, 387);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System.Drawing.Printing;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        PrintDocument printDocument = new PrintDocument();
+        DataTable table = new DataTable();
+        DataTable table2 = new DataTable();
+        DataSet ds = new DataSet();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*DataGridViewComboBoxColumn cmb = new DataGridViewComboBoxColumn();
+            cmb.Items.Add("terma");
+            cmb.Items.Add("tabon");
+            dataGridView1.Columns.Add(cmb);
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.Text = "hawi";
+            btn.HeaderText = "click for fuck";
+            btn.Name = "btn";
+            dataGridView1.Columns.Add(btn);*/
+            /*ComboBox comboBoxHeaderCell1 = new ComboBox();
+            comboBoxHeaderCell1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxHeaderCell1.Visible = true;
+            comboBoxHeaderCell1.Items.Add("Column1");
+            comboBoxHeaderCell1.Items.Add("Column2");
+            dataGridView1.Controls.Add(comboBoxHeaderCell1);
+            comboBoxHeaderCell1.Location = this.dataGridView1.GetCellDisplayRectangle(1, -1, true).Location;
+            comboBoxHeaderCell1.Size = this.dataGridView1.Columns[0].HeaderCell.Size;
+            comboBoxHeaderCell1.Text = "Column1";*/
+
+            printPreviewDialog1.ShowDialog();
+
+        }
+
+        private void pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            table = new DataTable();
+            table.Columns.Add(new DataColumn("id", typeof(int)));
+            table.Columns.Add(new DataColumn("nom", typeof(string)));
+            for(int i = 0; i < 20; i++)
+            {
+                table.Rows.Add(i, "abdu");
+            }
+            dataGridView1.DataSource = table;
+
+        }
+
+        private void afficher()
+        {
+            table = new DataTable();
+            table.Columns.Add(new DataColumn("id",typeof(int)));
+            table.Columns.Add(new DataColumn("nom",typeof(string)));
+            table.Rows.Add(1, "abdu");
+            table.Rows.Add(2, "gg");
+            table.Rows.Add(1, "ftr");
+            table.Rows.Add(3, "rob");
+
+
+            
+            table2.Columns.Add(new DataColumn("id", typeof(int)));
+            table2.Rows.Add(1);
+            table2.Rows.Add(2);
+            table2.Rows.Add(3);
+            
+            ds.Tables.Add(table);
+            ds.Tables.Add(table2);
+
+            ds.Relations.Add("rl", ds.Tables[1].Columns[0], ds.Tables[0].Columns[0]);
+            foreach(DataRow rowdt in table2.Rows)
+            {
+                comboBox1.Items.Add(rowdt[0]);
+            }
+
+
+         
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(CustomMsgBox msgBox = new CustomMsgBox())
+            {
+                msgBox.ShowDialog();
+                string result = msgBox.id;
+                MessageBox.Show(result);
+                DataView dt = new DataView(table, "nom = '1'", "type = Desc", DataViewRowState.CurrentRows);
+                //dataGridView1.DataSource = dt;
+
+            }
+            //((ToolStripMenuItem)contextMenuStrip1.Items[0]).Checked = true; //false;
+            redToolStripMenuItem.Checked = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataRow[] rows = ds.Tables[1].Rows[comboBox1.SelectedIndex].GetChildRows("rl");
+            DataTable tempTble = new DataTable();
+            tempTble.Columns.Add("id");
+            tempTble.Columns.Add("nom");
+            foreach(DataRow row in rows)
+            {
+                tempTble.Rows.Add(row[0],row[1]);
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = tempTble;
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Black, 20, 20, e.PageBounds.Width - 40, e.PageBounds.Height - 40);
+            float mrg = 20;
+            float colHeight = 60;
+            e.Graphics.DrawLine(Pens.Black, mrg, mrg+colHeight, e.PageBounds.Width - mrg, mrg+colHeight);
+            float col1Width = (e.PageBounds.Width - 40) / 2;
+            e.Graphics.DrawLine(Pens.Black, mrg+col1Width, mrg, mrg+col1Width, e.PageBounds.Height - mrg);
+        }
+    }
+}
